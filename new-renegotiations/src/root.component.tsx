4ae39687b11173973c4f995cb95e73aab46c2557
Marcos Felipe <0709.marcoscouto@gmail.com>
2024-05-24 20:29:34 -0300

    :recycle: refactor: change root app css

--- new-renegotiations/src/root.component.tsx
+++ new-renegotiations/src/root.component.tsx
@@ -1,3 +1,31 @@
+import "./root.css";
+
 export default function Root(props) {
-  return <span>React application is mounted!</span>;
+  return <>
+    <main>
+      <div className="phone-screen">
+        <header>
+          <button className="back-button">
+            <span className="icon">&lt;</span>
+            
+            <h1>renegociação</h1>
+          </button>
+        </header>
+
+        <section>
+          <div className="not-found-image">
+
+          </div>
+
+          <span className="text">
+            No momento, não encontramos contratos disponíveis para renegociar
+          </span>
+
+          <button>
+            Ir para a tela inicial
+          </button>
+        </section>
+      </div>
+    </main>
+  </>
 }
