84a4f5b01a36f09327fabfa4d4d4e69801057014
Marcos Felipe <0709.marcoscouto@gmail.com>
2024-05-24 19:02:33 -0300

    :sparkles: feat: empty angular component

--- my-renegotiations/src/app/app-routing.module.ts
+++ my-renegotiations/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
-import { APP_BASE_HREF } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { EmptyRouteComponent } from './empty-route/empty-route.component'
+import { EmptyRouteComponent } from './empty-route/empty-route.component';
+import { APP_BASE_HREF } from '@angular/common';
 
 const routes: Routes = [
   { path: '**', component: EmptyRouteComponent }
@@ -10,6 +10,8 @@ const routes: Routes = [
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
-  providers: [{ provide: APP_BASE_HREF, useValue: '/' }]
+  providers: [
+    { provide: APP_BASE_HREF, useValue: '/' }
+  ]
 })
 export class AppRoutingModule { }
