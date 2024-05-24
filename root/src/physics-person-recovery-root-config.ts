ae8741c8c1617869175276840be450a4eb173dbc
Marcos Felipe <0709.marcoscouto@gmail.com>
2024-05-24 20:12:19 -0300

    :sparkles: feat: create root application base layout

--- root/src/physics-person-recovery-root-config.ts
+++ root/src/physics-person-recovery-root-config.ts
@@ -1,18 +1,17 @@
 import { registerApplication, start } from "single-spa";
-import {
-  constructApplications,
-  constructRoutes,
-  constructLayoutEngine,
-} from "single-spa-layout";
+import { constructApplications, constructRoutes, constructLayoutEngine } from "single-spa-layout";
+import "./microfrontend-layout.css";
 import microfrontendLayout from "./microfrontend-layout.html";
 
 const routes = constructRoutes(microfrontendLayout);
+
 const applications = constructApplications({
   routes,
   loadApp({ name }) {
     return System.import(name);
   },
 });
+
 const layoutEngine = constructLayoutEngine({ routes, applications });
 
 applications.forEach(registerApplication);
