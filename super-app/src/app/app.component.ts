e33b0b203cd59e27c860fa5f841cef837231dc53
Marcos Felipe <0709.marcoscouto@gmail.com>
2024-05-24 23:15:50 -0300

    :sparkles: feat: super app project created

---
+++ super-app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.css']
+})
+export class AppComponent {
+  title = 'super-app';
+}
